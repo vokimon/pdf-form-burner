f40cd5b15fe00ee94adfdd4455d0013fac22dcf9
David García Garzón <voki@canvoki.net>
2014-07-16 16:26:42 +0200

    Using yaml-cpp to dump yaml

--- SConstruct
+++ SConstruct
@@ -1,7 +1,7 @@
 env = Environment()
 
 
-env.ParseConfig('pkg-config --libs --cflags poppler')
+env.ParseConfig('pkg-config --libs --cflags poppler yaml-cpp')
 env.Append(CCFLAGS='-g')
 env.Program('pdfform', Glob("*.cc"))
 
--- pdfform.cc
+++ pdfform.cc
@@ -6,6 +6,7 @@
 #include <poppler/UnicodeMap.h>
 #include <poppler/UTF.h>
 #include <poppler/Dict.h>
+#include <yaml-cpp/yaml.h>
 
 #include <iostream>
 #include <string>
@@ -28,7 +29,7 @@ const char * buttonTypeStrings[] = {
 	"Radio",
 	0};
 
-std::string encode(GooString *s)
+std::string pdftext_2_utf8(GooString *s)
 {
 	if (!s) return "";
 	if (!s->getCString()) return "";
@@ -41,8 +42,99 @@ std::string encode(GooString *s)
 		int n = uMap->mapUnicode(u[i], buffer, sizeof(buffer));
 		output.append(buffer,n);
 	}
-	delete u;
+	free(u);
 	return output;
+}
+
+// Taken from 
+GooString * utf8_2_pdftext(const std::string & s)
+{
+	GooString * result = new GooString;
+	const char * in = s.c_str();
+	unsigned int codepoint;
+	result->append('\xfe');
+	result->append('\xff');
+	while (*in != 0)
+	{
+		unsigned char ch = static_cast<unsigned char>(*in);
+		if (ch <= 0x7f)
+			codepoint = ch;
+		else if (ch <= 0xbf)
+			codepoint = (codepoint << 6) | (ch & 0x3f);
+		else if (ch <= 0xdf)
+			codepoint = ch & 0x1f;
+		else if (ch <= 0xef)
+			codepoint = ch & 0x0f;
+		else
+			codepoint = ch & 0x07;
+		++in;
+		if (((*in & 0xc0) != 0x80) && (codepoint <= 0x10ffff))
+		{
+			if (codepoint > 0xffff)
+			{
+//				result->append(static_cast<wchar_t>(0xd800 + (codepoint >> 10),1));
+//				result->append(static_cast<wchar_t>(0xdc00 + (codepoint & 0x03ff)),1);
+
+				result->append(0xd8 + (codepoint>>18)); // MSB
+				result->append(0xff & (codepoint>>10)); // LSB
+				result->append(0xdc + ((codepoint>>8)&0x03)); // MSB
+				result->append(0xff & codepoint); // LSB
+			}
+			else if (codepoint < 0xd800 || codepoint >= 0xe000)
+			{
+				result->append(codepoint>>8);
+				result->append(codepoint&0xff);
+			}
+		}
+	}
+	return result;
+}
+
+
+int dumpPdfAsYaml(Form * form, std::ostream & outputfile)
+{
+	YAML::Emitter out(outputfile);
+	out << YAML::BeginMap;
+	for (unsigned i = 0; i < form->getNumFields(); i++)
+	{
+		FormField *field = form->getRootField(i);
+		FormFieldType type = field->getType();
+		std::string fieldName = pdftext_2_utf8(
+			field->getFullyQualifiedName());
+		out << YAML::Key << fieldName;
+		if (type == formText)
+		{
+			FormFieldText * textField = dynamic_cast<FormFieldText*>(field);
+			GooString * content = textField->getContent();
+			out << YAML::Value << (content?pdftext_2_utf8(content):"");
+			out << YAML::Comment(typeStrings[type]);
+			continue;
+		}
+		if (type == formChoice)
+		{
+			FormFieldChoice * choiceField = dynamic_cast<FormFieldChoice*>(field);
+			GooString * content = choiceField->getSelectedChoice();
+			out << YAML::Value << (content?pdftext_2_utf8(content):"");
+			std::ostringstream os;
+			os << typeStrings[type] << ": ";
+			for (unsigned i = 0; i<choiceField->getNumChoices(); i++)
+				os << (i?", ":"") << pdftext_2_utf8(choiceField->getChoice(i));
+			out << YAML::Comment(os.str());
+			continue;
+		}
+		{
+			out << YAML::Value << YAML::Null;
+			out << YAML::Comment(
+				"Ignored  Field: '" + fieldName + "' Type: " + typeStrings[type])
+				;
+			std::cerr
+				<< "Ignored  Field: '" << fieldName
+				<< "' Type: " << typeStrings[type]
+				<< std::endl;
+		}
+	}
+	out << YAML::EndMap;
+	return 0;
 }
 
 
@@ -81,6 +173,8 @@ int main(int argc, char** argv)
 
 	PDFDoc * doc = PDFDocFactory().createPDFDoc(pdfFileName);
 
+	if (not doc) return error("Unable to open document");
+
 	// TODO: check doc
 	unsigned nEmbedded = doc->getCatalog()->numEmbeddedFiles();
 	std::cout << "Embedded Files: " << nEmbedded << std::endl;
@@ -96,30 +190,43 @@ int main(int argc, char** argv)
 		return error("PDF form format not supported");
 
 	Form *form = doc->getCatalog()->getForm();
-	for (unsigned i = 0; i < form->getNumFields(); i++)
+
+	dumpPdfAsYaml(form, std::cout);
+
+	for (unsigned i = 0; false && i < form->getNumFields(); i++)
 	{
 		FormField *field = form->getRootField(i);
 		GooString *qualifiedName = field->getFullyQualifiedName();
 		FormFieldType type = field->getType();
-		std::cout
-			<< "Field: '" << qualifiedName->getCString() << "'\n"
-			<<"\tType: " << typeStrings[type] << std::endl;
 		if (type == formText)
 		{
+			// TODO: Escape name
 			FormFieldText * textField = dynamic_cast<FormFieldText*>(field);
 			GooString * content = textField->getContentCopy();
+			std::cout
+				<< qualifiedName->getCString() << ": "
+				;
 			if (content)
 			{
-				std::cout << "\tContent: '" << encode(content).c_str() << "'" << std::endl;
+				std::cout
+					<< "'" << pdftext_2_utf8(content).c_str() << "'"
+					;
 				delete content;
 			}
+			std::cout
+				<< " # " << typeStrings[type]
+				<< std::endl;
 
-			const char value[] = "\xfe\xff\0T\0o\0m\0a\0";
-			GooString *newValue = new GooString(value,sizeof(value));
+			GooString *newValue = utf8_2_pdftext("toooomá ñacata");
+
 			textField->setContentCopy(newValue);
 			delete newValue;
-
-
+		}
+		else
+		{
+			std::cout
+				<< "Field: '" << qualifiedName->getCString() << "'\n"
+				<<"\tType: " << typeStrings[type] << std::endl;
 		}
 	}
 	GooString * outputFilename = new GooString(argv[2]);
